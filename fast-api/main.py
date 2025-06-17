0a2112b23ee7de597f5cd6ee574940a81f4427dc
Siobhan Doherty <siobhandoherty18@gmail.com>
2025-06-17 12:44:08 +0000

    Verify Part C: request body in 'FastAPI - pass the data to the API'

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,27 +1,15 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 
 api = FastAPI()
 
-@api.get("/")
-def get_index(argument1):
-    return {
-        "data": argument1
-    }
+class Item(BaseModel):
+    itemid: int
+    description: str
+    owner: Optional[str] = None
 
-@api.get("/typed")
-def get_typed(argument1: int):
-    return {
-        "data": argument1 + 1
-    }
-
-@api.get("/addition")
-def get_addition(a: int, b: Optional[int] = None):
-    if b:
-        result = a + b
-    else:
-        result = a + 1
-    return {
-        "addition_result": result
-    }
+@api.post('/item')
+def post_item(item: Item):
+    return item
