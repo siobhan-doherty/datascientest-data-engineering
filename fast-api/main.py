d2e610f106382d8dc2beb7834c2eae65dbde4f03
Siobhan Doherty <siobhandoherty18@gmail.com>
2025-06-18 14:50:56 +0000

    d. part 2 using errors in 'FastAPI - Improve the documentation'

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI # type: ignore
+from fastapi import FastAPI, HTTPException # type: ignore
 
 
 api = FastAPI()
@@ -6,6 +6,17 @@ data = [1, 2, 3, 4, 5]
 
 @api.get("/data")
 def get_data(index):
-    return {
-        "data": data[int(index)]
-    }
+    try:
+        return {
+            "data": data[int(index)]
+        }
+    except IndexError:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Unknown Index"
+        )
+    except ValueError:
+        raise HTTPException(
+            status_code = 400,
+            detail = "Bad Type"
+        )
