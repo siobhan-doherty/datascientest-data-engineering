9c930e3b1f3d18d2d437031767b6ec079519bd0b
Siobhan Doherty <siobhandoherty18@gmail.com>
2025-06-18 15:12:00 +0000

    d. part 3 using errors in 'FastAPI - Improve the documentation'

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,22 +1,41 @@
-from fastapi import FastAPI, HTTPException # type: ignore
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+import datetime
 
 
 api = FastAPI()
-data = [1, 2, 3, 4, 5]
 
-@api.get("/data")
-def get_data(index):
-    try:
-        return {
-            "data": data[int(index)]
+class MyException(Exception):
+    def __init__(self, name: str, date: str):
+        self.name = name
+        self.date = date
+
+@api.exception_handler(MyException)
+def MyExceptionHandler(request: Request, exception: MyException):
+    return JSONResponse(
+        status_code = 418,
+        content = {
+            "url": str(request.url),
+            "name": exception.name,
+            "message": "This error is my own", 
+            "date": exception.date
         }
-    except IndexError:
-        raise HTTPException(
-            status_code = 404,
-            detail = "Unknown Index"
-        )
-    except ValueError:
-        raise HTTPException(
-            status_code = 400,
-            detail = "Bad Type"
-        )
+    )
+
+@api.get("/my_custom_exception")
+def get_my_custom_exception():
+    raise MyException(
+        name = "my error",
+        date = str(datetime.datetime.now())
+    )
+
+responses = {
+    200: {"description": "OK"},
+    404: {"description": "Item not found"},
+    302: {"description": "The item was moved"},
+    403: {"description": "Not enough privileges"},
+}
+
+@api.get("/thing", responses = responses)
+def get_thing():
+    return {"data": "hello world"}
