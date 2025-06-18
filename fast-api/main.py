e57473ed57bc92f1c38b81fe1a7b4eadeb6d5729
Siobhan Doherty <siobhandoherty18@gmail.com>
2025-06-18 12:53:31 +0000

    verify Part B: documentation of the function body in 'FastAPI - Improve the documentation'

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI # type: ignore
+from pydantic import BaseModel # type: ignore
+from typing import Optional
+from fastapi import FastAPI, Header  # type: ignore
 
 
 api = FastAPI(
@@ -7,7 +9,20 @@ api = FastAPI(
     version = "1.0.1"
 )
 
-@api.get("/", name = "Hello World")
-def get_index():
-    """Returns greetings"""
-    return {"greetings": "welcome"}
+class Computer(BaseModel):
+    """a computer that is available in the store"""
+    computerid: int
+    cpu: Optional[str]
+    gpu: Optional[str]
+    price: float
+
+@api.put("/computer", name = "Create a new computer")
+def get_computer(computer: Computer):
+    """Creates a new computer within the database"""
+    return computer
+
+@api.get("/custom", name = "Get custom header")
+def get_content(custom_header: Optional[str] = Header(None, description = "My own personal header")):
+    return {
+        "Custom-Header": custom_header
+    }
