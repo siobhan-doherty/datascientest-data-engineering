adfcb4d6cc3ff5cc115cb9c4a410703b6792de37
Siobhan Doherty <siobhandoherty18@gmail.com>
2025-07-11 12:01:27 +0000

    Dash - Introduction and Installation

---
+++ dash/my_first_dash_app.py
@@ -0,0 +1,16 @@
+import dash
+from dash import dcc, html
+
+
+# instantiate Dash app
+app = dash.Dash(__name__)
+# define app layout (appearance in browser)
+app.layout = html.Div(children = "First API Dash")
+
+# run dev server
+if __name__ == "__main__":  
+    app.run(
+        debug = True,
+        host = "0.0.0.0",
+        port = 5000
+    )
